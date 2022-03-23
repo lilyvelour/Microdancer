58ee81ad145d7982604bae39ffa5e4bcaa491c11
lilyvelour <1100891+lilyvelour@users.noreply.github.com>
2022-03-22 19:24:15 -0500

    Use a .NET friendly version number

---
+++ get-version.ps1
@@ -0,0 +1,11 @@
+$tag = Invoke-Expression "git describe --tags --always"
+$regex = [regex]"v\d+\.\d+\.\d+\-?\d*"
+$match = $regex.Match($tag)
+$result = $match.Captures[0].value
+$result = $result -replace '-', '.'
+$result = $result -replace 'v', ''
+$charCount = ($result.ToCharArray() | Where-Object { $_ -eq '.' } | Measure-Object).Count
+if ( $charCount -eq 2 ) {
+    $result += '.0'
+}
+Write-Host $result
